f6eac45881dfaf282bb52101ee3944917e61e810
durgesh patel <dfordurgesh05@gmail.com>
2025-11-10 03:16:35 -1200

    Update assignment_Q_1.py

--- assignment_Q_1.py
+++ assignment_Q_1.py
@@ -1,6 +1,6 @@
 # A wire is the form of Arc at an angle of 60 degrees and
 # the radius of the arc is 42. The wire is converted into a square. Find the area of the square.
-angle = int(input("Enter angle: "))
+angle = int(input("Enter angle: ")) #input angle
 radius = int(input("Radius: "))
 pi = 3.14
 lengthOfWire = (angle / 360)*2*pi*radius
